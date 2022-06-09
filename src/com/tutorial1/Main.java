0cd67c025cec52a6e8a8cc5cc152e249d9d91e6d
Ryan Abraham <ryan.ryanabraham@gmail.com>
2022-06-09 10:28:52 -0700

    Main Commit

---
+++ src/com/tutorial1/Main.java
@@ -0,0 +1,19 @@
+package com.tutorial1;
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Principal: ");
+        int principal = scanner.nextInt();
+        System.out.print("Rate: ");
+        double rate = scanner.nextDouble() / 100 / 12;
+        System.out.print("Time: ");;
+        int time = scanner.nextByte();
+        time = time*12;
+        String formatter = NumberFormat.getCurrencyInstance().format(principal*(rate*Math.pow(1+rate, time))/(Math.pow(1+rate, time)-1));
+        System.out.println("Total: " + formatter);
+    }
+}
