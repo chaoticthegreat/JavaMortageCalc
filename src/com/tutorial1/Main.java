7ef821708b0c3668afcdc0b373b7c3e31d3e67b2
Ryan Abraham <ryan.ryanabraham@gmail.com>
2022-06-10 11:29:54 -0700

    Added principal checking system

--- src/com/tutorial1/Main.java
+++ src/com/tutorial1/Main.java
@@ -6,8 +6,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
+        int principal = 0;
+        while (!(principal >= 1000) || !(principal <= 1000000)) {
+            System.out.print("Principal: ");
+            principal = scanner.nextInt();}
         System.out.print("Rate: ");
         double rate = scanner.nextDouble() / 100 / 12;
         System.out.print("Time: ");;
