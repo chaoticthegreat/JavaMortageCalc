3cd0de93aee16955bfc900a4c564d6140850e841
Ryan Abraham <ryan.ryanabraham@gmail.com>
2022-06-10 11:49:15 -0700

    Minimum, Maximum value for each input, fixed last

--- src/com/tutorial1/Main.java
+++ src/com/tutorial1/Main.java
@@ -9,13 +9,40 @@ public class Main {
         int principal = 0;
         while (!(principal >= 1000) || !(principal <= 1000000)) {
             System.out.print("Principal: ");
-            principal = scanner.nextInt();}
-        System.out.print("Rate: ");
-        double rate = scanner.nextDouble() / 100 / 12;
-        System.out.print("Time: ");;
-        int time = scanner.nextByte();
-        time = time*12;
-        String formatter = NumberFormat.getCurrencyInstance().format(principal*(rate*Math.pow(1+rate, time))/(Math.pow(1+rate, time)-1));
+            principal = scanner.nextInt();
+            if (principal < 1000) {
+                System.out.println("Principal must be at least 1000");
+            } else if (principal > 1000000) {
+                System.out.println("Principal must be at most 1000000");
+            } else {
+                break;
+            }
+        }
+        double rate = 0.00;
+        while (!(rate >= 1) || !(rate <= 30)) {
+            System.out.print("Rate: ");
+            rate = scanner.nextDouble();
+            if (rate < 1) {
+                System.out.println("Rate must be at least 1");
+            } else if (rate > 30) {
+                System.out.println("Rate must be at most 30");
+            } else {
+                break;
+            }
+        }
+        rate = rate / 100 / 12;
+        int years = 0;
+        while (true) {
+            System.out.print("Number of years: ");
+            years = scanner.nextInt();
+            if (years < 1) {
+                System.out.println("Number of years must be at least 1");
+            } else {
+                break;
+            }
+        }
+        int months = years*12;
+        String formatter = NumberFormat.getCurrencyInstance().format(principal*(rate*Math.pow(1+rate, months))/(Math.pow(1+rate, months)-1));
         System.out.println("Total: " + formatter);
         scanner.close();
     }
